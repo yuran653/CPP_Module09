bda2a874f0a64dddebdb8eb7cc3449d799c73132
Iurii Ovsiannikov <jgoldste@z2t2c2.42bangkok.com>
2023-12-05 22:16:14 +0700

    ex00_Dec5_22-16

--- ex00/include/BitcoinExchange.hpp
+++ ex00/include/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:52:03 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/12/05 15:51:55 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/12/05 21:28:58 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 #include <iostream>
 #include <limits>
 #include <map>
+#include <queue>
 #include <sstream>
 #include <string>
 #include <utility>
@@ -31,17 +32,28 @@
 #define INPUT_DELIM '|'
 #define DATA_HEAD "date,exchange_rate"
 #define INPUT_HEAD "date | value"
+#define DATA_FILE "data.csv"
+#define INPUT_FILE "input.txt"
 
 class BitcoinExchange {
 	private:
 		static std::multimap<int, long double>* _data;
-		static std::multimap<int, long double>* _input;
+		// static std::multimap<int, long double>* _input;
+		static std::queue<std::pair<int, long double> >* _input;
 
 		static void _initialize();
 		static void _cleanup();
-		static void _openReadFile
-			(std::string file_name, std::multimap<int, long double>* container, char delim);
+		// static void _openReadFile
+		// 	(std::string file_name, std::multimap<int, long double>* container, char delim);
+		template <typename T>
+		static void _openReadFile (std::string file_name, T* container, char delim);
+		static void _addPair(std::multimap<int, long double>* container, std::string buffer, char delim);
+		static void _addPair(std::queue<std::pair<int, long double> >* container, std::string buffer, char delim);
+		static void _deleteHead(std::multimap<int, long double>* container);
+		static void _deleteHead(std::queue<std::pair<int, long double> >* container);
 		static std::pair<int, long double> _parseBufferData(std::string buffer, char delim);
+		// static std::pair<int, long double> _findRate(int key);
+		static std::string _printDate(int key);
 
 		BitcoinExchange();
 		~BitcoinExchange();
--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -6,14 +6,15 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:51:23 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/12/05 15:53:46 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/12/05 22:15:11 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
 std::multimap<int, long double>*	BitcoinExchange::_data;
-std::multimap<int, long double>*	BitcoinExchange::_input;
+// std::multimap<int, long double>*	BitcoinExchange::_input;
+std::queue<std::pair<int, long double> >* BitcoinExchange::_input;
 
 BitcoinExchange::BitcoinExchange() {
 }
@@ -23,7 +24,8 @@ BitcoinExchange::~BitcoinExchange() {
 
 void BitcoinExchange::_initialize() {
 	_data = new std::multimap<int, long double>;
-	_input = new std::multimap<int, long double>;
+	// _input = new std::multimap<int, long double>;
+	_input = new std::queue<std::pair<int, long double> >;
 }
 void BitcoinExchange::_cleanup() {
 	delete _data;
@@ -55,25 +57,82 @@ std::pair<int, long double> BitcoinExchange::_parseBufferData(std::string buffer
 	return (std::make_pair(key, value));
 }
 
-void BitcoinExchange::_openReadFile
-	(std::string file_name, std::multimap<int, long double>* container, char delim) {
+// void BitcoinExchange::_openReadFile
+// 	(std::string file_name, std::multimap<int, long double>* container, char delim) {
+// 	std::ifstream is(file_name);
+// 	std::string buffer;
+// 	if (is.is_open() == false)
+// 		throw OpenFileError("Can't open file: " + file_name);
+// 	while (std::getline(is, buffer)) {
+// 		container->insert(_parseBufferData(buffer, delim));
+// 	}
+// 	is.close();
+// 	container->erase(container->begin());
+// }
+
+void BitcoinExchange::_addPair(std::multimap<int, long double>* container, std::string buffer, char delim) {
+	container->insert(_parseBufferData(buffer, delim));
+}
+
+void BitcoinExchange::_addPair(std::queue<std::pair<int, long double> >* container, std::string buffer, char delim) {
+	container->push(_parseBufferData(buffer, delim));
+}
+
+void BitcoinExchange::_deleteHead(std::multimap<int, long double>* container) {
+	container->erase(container->begin());
+}
+
+void BitcoinExchange::_deleteHead(std::queue<std::pair<int, long double> >* container) {
+	container->pop();
+}
+
+template <typename T>
+void BitcoinExchange::_openReadFile (std::string file_name, T* container, char delim) {
 	std::ifstream is(file_name);
 	std::string buffer;
 	if (is.is_open() == false)
 		throw OpenFileError("Can't open file: " + file_name);
 	while (std::getline(is, buffer)) {
-		container->insert(_parseBufferData(buffer, delim));
+		_addPair(container, buffer, delim);
 	}
 	is.close();
+	_deleteHead(container);
+}
+
+// std::pair<int, long double> BitcoinExchange::_findRate(int key) {
+// 	for (std::multimap<int, long double>::iterator it = _data->begin(); it != _data->end(); it++) {
+
+// 	}
+// }
+
+#include <cmath>
+std::string BitcoinExchange::_printDate(int key) {
+	std::ostringstream oss;
+	oss << key;
+	std::string date = oss.str();
+	date.insert(4, "-");
+	date.insert(7, "-");
+	return date;
 }
 
 void BitcoinExchange::printData() {
 	_initialize();
-	_openReadFile("data.csv", _data, DATA_DELIM);
-	for (std::multimap<int, long double>::iterator it = _data->begin(); it != _data->end(); it++)
-		std::cout << it->first << " | " << std::fixed << std::setprecision(2) << it->second <<std::endl;
-	_openReadFile("input.txt", _input, INPUT_DELIM);
-	for (std::multimap<int, long double>::iterator it = _input->begin(); it != _input->end(); it++)
-		std::cout << it->first << " | " << std::fixed << std::setprecision(2) << it->second <<std::endl;
+	try {
+		_openReadFile(INPUT_FILE, _input, INPUT_DELIM);
+		_openReadFile(DATA_FILE, _data, DATA_DELIM);
+	} catch (const OpenFileError& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+	while (_input->size()) {
+		std::cout << _printDate(_input->front().first) << std::endl;
+		_input->pop();
+	}
+	// while (_input->size()) {
+	// 	std::cout << _input->front().first << " | " << _input->front().second << std::endl;
+	// 	_input->pop();
+	// }
+	// for (std::multimap<int, long double>::iterator it = _input->begin(); it != _input->end(); it++) {
+	// 	std::cout << _printDate(it->first) << std::endl;
+	// }
 	_cleanup();
 }
